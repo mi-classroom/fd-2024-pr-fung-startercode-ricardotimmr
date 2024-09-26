bc0d58019f53da11300194d3e619c6f21d28bfd9
ricardotimmr <ricardo.timmr@gmail.com>
2024-09-26 11:19:17 +0200

    F2 Ablauf einklappmachen aktuelles Stand

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,14 @@ document.addEventListener('DOMContentLoaded', function() {
   hljs.highlightAll();
   slideshow();
 });
+
+document.querySelectorAll('.section h2').forEach(function(header) {
+  header.addEventListener('click', function() {
+    const content = this.nextElementSibling; // Get the content after the header
+    content.classList.toggle('open'); // Toggle the open class
+    content.style.display = content.style.display === 'block' ? 'none' : 'block'; // Toggle visibility
+
+    // Toggle the active state of the header
+    this.classList.toggle('active');
+  });
+});
