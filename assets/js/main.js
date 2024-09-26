23cd47a4e525c095c1a38f56e60f21bbc52e6c79
ricardotimmr <ricardo.timmr@gmail.com>
2024-09-26 13:52:13 +0200

    F7 Bildstrecke für eine abgeschlossene Arbeit erstellen

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,36 @@ document.querySelectorAll('.section h2').forEach(function(header) {
       this.classList.toggle('active');
     });
   });
+
+const imageGallery = document.querySelector('.image-gallery');
+const imageDir = '/works/n-pola/04-results/images/';
+
+const imageFiles = [
+  'polarek-nils-ba-ss24-bilddokumentation-01-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-02-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-03-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-04-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-05-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-06-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-07-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-08-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-09-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-10-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-11-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-12-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-13-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-14-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-15-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-16-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-17-s.webp',
+  'polarek-nils-ba-ss24-bilddokumentation-18-s.webp'
+];
+
+// Loop through each image file and create an <img> element for it
+imageFiles.forEach(function(imageFile) {
+  const img = document.createElement('img');
+  img.src = `${imageDir}${imageFile}`;
+  img.alt = `Bild aus dem Projektverlauf - ${imageFile}`;
+  img.classList.add('gallery-image');
+  imageGallery.appendChild(img);
+});
