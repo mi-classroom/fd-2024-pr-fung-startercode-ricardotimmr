356c748408cc2a3ba81da296eb00ab7e40fcef5b
ricardotimmr <ricardo.timmr@gmail.com>
2024-09-26 14:22:17 +0200

    F2 neuester Stand

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,19 @@ document.addEventListener('DOMContentLoaded', function() {
   slideshow();
 });
 
-document.querySelectorAll('.section h2').forEach(function(header) {
-    header.addEventListener('click', function() {
-      const content = this.nextElementSibling; // Get the content after the header
+document.querySelectorAll('h2[id="vor-der-arbeit"], h2[id="während-der-arbeit"], h2[id="abschluss-der-arbeit"]').forEach(function(header) {
+  header.addEventListener('click', function() {
+    const content = this.nextElementSibling; // Get the content after the header
+    if (content.classList.contains('collapsible')) {
       content.classList.toggle('open'); // Toggle the open class
       content.style.display = content.style.display === 'block' ? 'none' : 'block'; // Toggle visibility
 
       // Toggle the active state of the header
       this.classList.toggle('active');
-    });
+    }
   });
+});
+
 
 
   // Image gallery leider keinen weg gefunden die dynamisch aus dem ordner zu fetchen
